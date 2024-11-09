46afaba77238b000cc61fc7d0115debd032b2f82
Khurram Ali <khurramcali@gmail.com>
2024-11-09 00:21:06 -0500

    with-auth init

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="container flex h-full items-center justify-between pt-12">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+    </div>
+  );
+}
