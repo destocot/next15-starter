522ae66675ef3eeae35b3964cd3307c23f9eca29
Khurram Ali <khurramcali@gmail.com>
2024-11-28 00:18:51 -0500

    init

---
+++ src/components/header.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { NavLink } from '@/components/nav-link'
+import { Button } from '@/components/ui/button'
+
+export const Header = () => {
+  return (
+    <header className='h-20 border-b'>
+      <div className='container flex h-full items-center'>
+        <Link href='/' className='text-3xl font-bold'>
+          NextStarter
+        </Link>
+
+        <nav className='flex flex-1 justify-end'>
+          <ul className='flex gap-x-2'>
+            <li>
+              <NavLink href='/' variant='ghost' size='sm'>
+                Home
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink href='/about' variant='ghost' size='sm'>
+                About
+              </NavLink>
+            </li>
+
+            <li>
+              <Button size='sm' asChild>
+                <Link href='/login'>Log In</Link>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
